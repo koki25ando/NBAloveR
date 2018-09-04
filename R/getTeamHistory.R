4f82c863126b9cbbe2e455079288d8a7df77ef43
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-04 19:53:48 +1000

    function for getting team history data

---
+++ R/getTeamHistory.R
@@ -0,0 +1,12 @@
+# getTeamHistory() function: Function that allows you to get team history data
+#' importFrom dplyr %>% 
+
+getTeamHistory <- function (team_code) {
+  url <- paste0("https://www.basketball-reference.com/teams/", str_to_upper(team_code), "/")
+  tables <- 
+    read_html(url) %>% 
+    html_table()
+  tables[[1]] %>% 
+    as.data.frame()
+}
+
