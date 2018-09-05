46ea9eb0b0e18f46d715b2e79bdd73f5df1b9eb2
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-05 18:18:19 +1000

    function specified

--- R/getStats.R
+++ R/getStats.R
@@ -6,10 +6,10 @@
 getStats <- function(){
   players_season_stats <- data.table::fread("https://s3-ap-southeast-2.amazonaws.com/playerinfomation/Seasons_Stats.csv", data.table = FALSE)
   players_season_stats %>% 
-    dplyr::select(-contains("WS"), -contains("STL%"), -contains("RB%"),-blanl,
-           -contains("BPM"), -contains("eFG%"), -contains("AST%"), -contains("BLK%"), 
-           -contains("TOV%"), -contains("USG%"), -blank2, -contains("TS%"), 
-           -contains("3PAr"), -VORP, -GS, -V1) %>% 
+    dplyr::select(-dplyr::contains("WS"), -dplyr::contains("STL%"), -dplyr::contains("RB%"),-blanl,
+           -dplyr::contains("BPM"), -dplyr::contains("eFG%"), -dplyr::contains("AST%"), -dplyr::contains("BLK%"), 
+           -dplyr::contains("TOV%"), -dplyr::contains("USG%"), -blank2, -dplyr::contains("TS%"), 
+           -dplyr::contains("3PAr"), -VORP, -GS, -V1) %>% 
     dplyr::group_by(Player, Year) %>% 
     dplyr::mutate(PPG = sum(PTS)/sum(G), RPG = sum(TRB)/sum(G), APG = sum(AST)/sum(G), SPG = sum(STL)/sum(G))
 }
--- R/getStatsSummary.R
+++ R/getStatsSummary.R
@@ -8,15 +8,15 @@ getStatsSummary <- function (Name) {
   players_season_stats <- data.table::fread("https://s3-ap-southeast-2.amazonaws.com/playerinfomation/Seasons_Stats.csv", data.table = FALSE)
   players_season_stats$Player <- stringr::str_remove(players_season_stats$Player, "\\*")
   stats_summary <- players_season_stats %>% 
-    dplyr::select(-contains("WS"), -contains("STL%"), -contains("RB%"),-blanl,
-           -contains("BPM"), -contains("eFG%"), -contains("AST%"), -contains("BLK%"), 
-           -contains("TOV%"), -contains("USG%"), -blank2, -contains("TS%"), 
-           -contains("3PAr"), -VORP, -GS, -V1) %>% 
+    dplyr::select(-dplyr::contains("WS"), -dplyr::contains("STL%"), -dplyr::contains("RB%"),-blanl,
+           -dplyr::contains("BPM"), -dplyr::contains("eFG%"), -dplyr::contains("AST%"), -dplyr::contains("BLK%"), 
+           -dplyr::contains("TOV%"), -dplyr::contains("USG%"), -dplyr::contains, -dplyr::contains("TS%"), 
+           -dplyr::contains("3PAr"), -VORP, -GS, -V1) %>% 
     dplyr::group_by(Player) %>% 
     dplyr::mutate(PPG = sum(PTS)/sum(G), RPG = sum(TRB)/sum(G), APG = sum(AST)/sum(G), SPG = sum(STL)/sum(G),
            Career = paste0(min(Year), "-", max(Year))) %>% 
     # select(Player, PPG:SPG) %>% 
-    dplyr::arrange(desc(Player)) %>% 
+    dplyr::arrange(dplyr::desc(Player)) %>% 
     distinct(Player, .keep_all = TRUE) %>% 
     dplyr::select(Player, Career, Pos, PPG:SPG) %>% 
     dplyr::filter(Player == Name)
