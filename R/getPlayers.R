3e5d374fa8aa6bcec5206029ef9b121e8537afb5
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-05 13:57:54 +1000

    code structures changes

--- R/getPlayers.R
+++ R/getPlayers.R
@@ -1,9 +1,7 @@
 # Function for getting all NBA players' information: name, heigh/weight, college, birth year, birth place
-
 #' @importFrom dplyr %>% 
 
 getPlayers <- function () {
   players_list_since1950 <- read.csv("https://s3-ap-southeast-2.amazonaws.com/koki25ando/Players.csv")
-  players_list_since1950 %>% 
-    select(Player:birth_state) 
+  dplyr::select(players_list_since1950, Player:birth_state) 
 }
