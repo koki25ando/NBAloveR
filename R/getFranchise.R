bef230a9adcb62198e34e2c21c633c3cc9ef7187
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-05 13:53:09 +1000

    changes

--- R/getFranchise.R
+++ R/getFranchise.R
@@ -1,5 +1,6 @@
 # Function for getting Franchises' data: founded year, W/L percentage, number of division/conf/league championship
 #' @importFrom magrittr %>%
+#' @export
 
 getFranchise <- function () {
   url <- "https://www.basketball-reference.com/teams/"
@@ -8,6 +9,6 @@ getFranchise <- function () {
     rvest::html_table()
   tables[[1]] %>% 
     as.data.frame() %>% 
-    filter(To == 2019) %>% 
+    dplyr::filter(To == 2019) %>% 
     distinct(Franchise, .keep_all=TRUE)
 }
