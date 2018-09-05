260d9c9dd69449b89077012e3dfe0bfac039cc5f
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-05 13:55:23 +1000

    remove a file

--- R/global.R
+++
@@ -1,7 +0,0 @@
-# Data Importing
-
-## Player list
-players_list_since1950 <- fread("https://s3-ap-southeast-2.amazonaws.com/koki25ando/Players.csv", data.table = FALSE)
-
-## Stats Data
-players_season_stats <- fread("https://s3-ap-southeast-2.amazonaws.com/playerinfomation/Seasons_Stats.csv", data.table = FALSE)
