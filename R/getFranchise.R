77398d2e8f0e4480b454356be15c4bcf7f3b8e37
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-08 16:54:39 +1000

    changes

--- R/getFranchise.R
+++ R/getFranchise.R
@@ -22,7 +22,7 @@ getFranchise <- function () {
   url <- "https://www.basketball-reference.com/teams/"
   page <- xml2::read_html(url)
   # tables <- rvest::html_table(page)
-  tables <- html_table(page)
+  tables <- rvest::html_table(page)
   table <- as.data.frame(tables[[1]])
   table <- dplyr::filter(table, To == 2019)
   table <- dplyr::distinct(table, Franchise, .keep_all=TRUE)
