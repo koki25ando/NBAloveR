9745f4475a2a5ce61f302153fa109e17faaaf8cc
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-25 21:26:32 +1000

    stats leader function added

---
+++ R/getStatsLeader.R
@@ -0,0 +1,15 @@
+# Function for getting stats leader
+
+getStatsLeader <- function (stats_type = c("PTS", "AST", "STL", "BLK", "FG2", "FG3"), RegularSeason = TRUE) {
+  base_url <- "https://www.basketball-reference.com/leaders/"
+  if (!RegularSeason) {
+    url <- paste0(base_url, stringr::str_to_lower(stats_type), "_season_p.html")
+  } else {
+    url <- paste0(base_url, stringr::str_to_lower(stats_type), "_season.html")
+  }
+  
+  tables <- read_html(as.character(url)) %>% 
+    html_table()
+  data.frame(tables[[1]])
+  
+}
