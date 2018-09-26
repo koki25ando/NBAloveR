9aeb5cf6aea9cc98501834b7de1fdb5c4c238b83
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-26 12:21:39 +1000

    function to get hofs

---
+++ R/getHOF.R
@@ -0,0 +1,8 @@
+# Function to get list of Hall of Famers
+
+getHOF <- function(){
+  url <- "https://en.wikipedia.org/wiki/List_of_players_in_the_Naismith_Memorial_Basketball_Hall_of_Fame"
+  tables <- read_html(url) %>% 
+    html_table(fill = TRUE)
+  data.frame(tables[[1]])[,1:4]
+}
