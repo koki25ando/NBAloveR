f89d9dd65e4fb3f8a5626fbb3fe632ca26544262
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-29 12:45:51 +1000

    bugs fixed

--- R/getDraftResult.R
+++ R/getDraftResult.R
@@ -2,21 +2,33 @@
 
 getDraftResult <- function (year) {
   
-  head_url <- "http://www.nbadraft.net/nba_draft_history/"
   tail_url <- ".html"
-  url <- paste0(head_url, as.character(year), tail_url)
+  
+  if (year >= 2009) {
+    head_url <- "http://www.nbadraft.net/nba_final_draft/"
+    url <-  paste0(head_url, as.character(year))
+  } else {
+    head_url <- "http://www.nbadraft.net/nba_draft_history/"
+    url <- paste0(head_url, as.character(year), tail_url)
+  }
   
   page <- read_html(url)
   tables <- page %>% 
     html_table()
   
-  if (year >= 2000) {
+  if (year >= 2009) {
+    table_1 <- tables[[1]][,c(1:3,7)]
+    table_2 <- tables[[1]][,c(9:11,15)]
+    
+    table <- bind_rows(table_1, table_2)
+    
+  } else if (year >= 2000) {
     table_1 <- tables[[1]][,c(1,3:4)]
     table_2 <- tables[[1]][,c(5,7:8)]
     names(table_1) <- c("No.", "Team", "Player")
     names(table_2) <- c("No.", "Team", "Player")
     
-    bind_rows(table_1, table_2)
+    table <- bind_rows(table_1, table_2)
     
   } else if (year == 1999) {
     table_1 <- tables[[1]]
@@ -31,18 +43,21 @@ getDraftResult <- function (year) {
       separate(Team, sep = "\n", into = c("No.", "Team")) %>% 
       unite(Player, College, sep = " ", col = "Player")
     
-    bind_rows(table_1, table_2)
+    table <- bind_rows(table_1, table_2)
+    
   } else {
     table_1 <- tables[[1]][-1,]
     table_2 <- tables[[2]][-1,]
     
-    bind_rows(table_1, table_2)
+    table <- bind_rows(table_1, table_2)
+    names(table) <- c("Team", "Player", "College")
   }
   
+  return(table)
+ 
 }
 
 
 # test
-getDraftResult(year = 2017)
+getDraftResult(year = 2009) %>% View()
 
-url <- "http://www.nbadraft.net/nba_draft_history/1990.html"
