35c1fd015bca9a1312788f03585264584a630727
koki25ando <36522188+koki25ando@users.noreply.github.com>
2018-12-15 09:35:00 +0900

    Delete TEST.R

--- TEST.R
+++
@@ -1,9 +0,0 @@
-# setwd("/Users/KokiAndo/desktop/R/NBAloveR")
-
-devtools::install_github("koki25ando/NBAloveR")
-# library(tidyverse)
-library(NBAloveR)
-
-NBAloveR::statsCompare(player_list = c("Kobe Bryant", "Allen Iverson", "Paul Pierce"))
-
-
