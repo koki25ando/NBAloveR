a13840e729a5d41c67825ff48605d5c77e8b1076
Koki Ando <basket.koki.bowl@gmail.com>
2019-01-03 21:09:49 +0900

    bigs fixed

--- R/getStatsSummary.R
+++ R/getStatsSummary.R
@@ -8,15 +8,17 @@
 #' 
 #' @export
 getStatsSummary <- function (Name) {
-  players_season_stats <- data.table::fread("https://s3-ap-southeast-2.amazonaws.com/playerinfomation/Seasons_Stats.csv", data.table = FALSE)
-  players_season_stats$Player <- stringr::str_remove(players_season_stats$Player, "\\*")
-  stats_summary <- players_season_stats %>% 
-    dplyr::select(Year:G, "FG%", "3P", "3P%", TRB:PTS) %>% 
-    dplyr::group_by(Player) %>% 
-    dplyr::mutate(PPG = sum(PTS)/sum(G), RPG = sum(TRB)/sum(G), APG = sum(AST)/sum(G), SPG = sum(STL)/sum(G),
-                  Career = paste0(min(Year), "-", max(Year))) %>% 
-    dplyr::arrange(dplyr::desc(Player)) %>% 
-    dplyr::distinct(Player, .keep_all = TRUE) %>% 
-    dplyr::select(Player, Career, Pos, PPG:SPG) %>% 
-    dplyr::filter(Player == Name)
+  head_url = "https://www.basketball-reference.com/players/"
+  tail_url = "01.html"
+  
+  player_key <- paste0(substr(strsplit(Name, " ")[[1]][2], 0,1), "/",
+                       substr(strsplit(Name, " ")[[1]][2], 1,5),
+                       substr(Name, 1,2)) %>%
+    stringr::str_to_lower()
+  
+  tables <- paste0(head_url, player_key, tail_url) %>%
+    xml2::read_html() %>%
+    rvest::html_table()
+  tables[[1]] %>% 
+    dplyr::filter(Season=="Career")
 }
