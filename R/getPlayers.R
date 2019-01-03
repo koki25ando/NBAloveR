3033c37bc9230a587669ce07ec447581bcc98da6
Koki Ando <basket.koki.bowl@gmail.com>
2019-01-03 19:28:12 +0900

    data modified

--- R/getPlayers.R
+++ R/getPlayers.R
@@ -15,6 +15,8 @@
 #' @export
 
 getPlayers <- function () {
-  players_list_since1950 <- read.csv("https://s3-ap-southeast-2.amazonaws.com/koki25ando/Players.csv")
-  dplyr::select(players_list_since1950, Player:birth_state)
+  Players <- utils::read.csv("data-raw/Players.csv")
+  Players
+  # players_list_since1950 <- read.csv("https://s3-ap-southeast-2.amazonaws.com/koki25ando/Players.csv")
+  # dplyr::select(players_list_since1950, Player:birth_state)
 }
