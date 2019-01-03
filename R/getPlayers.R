5cd9bdc638775d3561e9ee316d7f00d0042276cb
Koki Ando <basket.koki.bowl@gmail.com>
2019-01-03 19:34:22 +0900

    script change

--- R/getPlayers.R
+++ R/getPlayers.R
@@ -17,6 +17,4 @@
 getPlayers <- function () {
   Players <- utils::read.csv("data-raw/Players.csv")
   Players
-  # players_list_since1950 <- read.csv("https://s3-ap-southeast-2.amazonaws.com/koki25ando/Players.csv")
-  # dplyr::select(players_list_since1950, Player:birth_state)
 }
