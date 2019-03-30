47ac3cee62000e7038c50fc7c97d3e5b35b42d89
Koki Ando <basket.koki.bowl@gmail.com>
2019-03-30 12:06:28 +0900

    utility functions added

---
+++ R/util.R
@@ -0,0 +1,25 @@
+#' Util
+
+player_key_generation = function(input){
+  head_url = "https://www.basketball-reference.com/players/"
+  end_num = "01"
+  tail_url = paste0(substr(strsplit(input, " ")[[1]][2], 0,1), "/",
+                    substr(strsplit(input, " ")[[1]][2], 1,5),
+                    substr(strsplit(input, " ")[[1]][1], 0,2),
+                    end_num) %>% 
+    stringr::str_to_lower()
+  player_url = paste0(head_url, tail_url, ".html/")
+  
+  name_on_page = xml2::read_html(player_url) %>% 
+    rvest::html_nodes("div.players") %>% 
+    rvest::html_nodes("[itemprop = name]") %>% 
+    rvest::html_text()
+  
+  if (input == name_on_page){
+    player_key = tail_url
+  } else {
+    player_key = stringr::str_replace(tail_url, "01", "02")
+  }
+  
+  return(player_key)
+}
